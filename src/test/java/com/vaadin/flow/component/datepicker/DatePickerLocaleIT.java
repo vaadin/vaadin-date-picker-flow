bc23d92f6c26ee5f3deace1470bbce33ba2c80d3
Mikael Grankvist <mikael.grankvist@vaadin.com>
2018-06-28 13:13:32 +0300

    Immediately update oldLocale if no initial value set.
    If the oldLocale is not updated for the empty input then the
    first time a date is selected we receive a
    `Selected locale is using unsupported date format, which might affect the parsing date.`
    warning in the console for non default en-US locales.

--- src/test/java/com/vaadin/flow/component/datepicker/DatePickerLocaleIT.java
+++ src/test/java/com/vaadin/flow/component/datepicker/DatePickerLocaleIT.java
@@ -1,16 +1,21 @@
 package com.vaadin.flow.component.datepicker;
 
+import java.util.logging.Level;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.logging.LogEntries;
 
 import com.vaadin.flow.testutil.AbstractComponentIT;
 import com.vaadin.flow.testutil.TestPath;
 
 @TestPath("date-picker-locale")
 public class DatePickerLocaleIT extends AbstractComponentIT {
+
+    private static final String DATEPICKER_OVERLAY = "vaadin-date-picker-overlay";
 
     @Test
     public void testPickerWithValueAndLocaleFromServerSideDifferentCtor() {
@@ -34,5 +39,21 @@ public class DatePickerLocaleIT extends AbstractComponentIT {
         Assert.assertEquals("French locale date had wrong format",
                 "30/05/2018",
                 executeScript("return arguments[0].value", displayText));
+
+        LogEntries logs  =  driver.manage().logs().get("browser");
+
+        Assert.assertEquals("Expected only [Deprecation] warning should be in the logs", 1, logs.filter(Level.WARNING).size());
+
+        localePicker = findElement(By.id("german-locale-date-picker"));
+        executeScript("arguments[0].value = '10.01.1985'", localePicker);
+
+        logs  =  driver.manage().logs().get("browser");
+
+        Assert.assertEquals("Expected only [Deprecation] warning should be in the logs", 1, logs.filter(Level.WARNING).size());
+
+        displayText = findInShadowRoot(localePicker, By.id("input")).get(0);
+        Assert.assertEquals("Didn't have expected German locale date.", "23.04.2018",
+                executeScript("return arguments[0].value", displayText));
+
     }
 }
