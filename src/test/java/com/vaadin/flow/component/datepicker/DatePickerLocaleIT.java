51c110b17a6a65b787e5522211946288d036b567
Mikael Grankvist <mikael.grankvist@vaadin.com>
2018-06-28 14:24:25 +0300

    Fix faulty values, update to TestbenchElement

--- src/test/java/com/vaadin/flow/component/datepicker/DatePickerLocaleIT.java
+++ src/test/java/com/vaadin/flow/component/datepicker/DatePickerLocaleIT.java
@@ -3,7 +3,6 @@ package com.vaadin.flow.component.datepicker;
 import java.util.logging.Level;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -11,6 +10,7 @@ import org.openqa.selenium.logging.LogEntries;
 
 import com.vaadin.flow.testutil.AbstractComponentIT;
 import com.vaadin.flow.testutil.TestPath;
+import com.vaadin.testbench.TestBenchElement;
 
 @TestPath("date-picker-locale")
 public class DatePickerLocaleIT extends AbstractComponentIT {
@@ -21,39 +21,46 @@ public class DatePickerLocaleIT extends AbstractComponentIT {
     public void testPickerWithValueAndLocaleFromServerSideDifferentCtor() {
         open();
 
-        WebElement localePicker = findElement(
-                By.id("locale-picker-server-with-value"));
-        WebElement displayText = findInShadowRoot(localePicker, By.id("input"))
-                .get(0);
+        TestBenchElement localePicker = $(TestBenchElement.class).id("locale-picker-server-with-value");
+        WebElement displayText = localePicker.$(TestBenchElement.class).id("input");
 
         Assert.assertEquals("Wrong initial date in field.", "2018/4/23",
                 executeScript("return arguments[0].value", displayText));
 
         findElement(By.id("uk-locale")).click();
-        Assert.assertEquals("Didn't have expected UK locale date.", "23/04/2018",
+        Assert.assertEquals("Didn't have expected UK locale date.",
+                "23/04/2018",
                 executeScript("return arguments[0].value", displayText));
 
-        localePicker = findElement(By.id("french-locale-date-picker"));
-        displayText = findInShadowRoot(localePicker, By.id("input")).get(0);
+        localePicker = $(TestBenchElement.class).id("french-locale-date-picker");
+        displayText = localePicker.$(TestBenchElement.class).id("input");
 
-        Assert.assertEquals("French locale date had wrong format",
-                "30/05/2018",
+        Assert.assertEquals("French locale date had wrong format", "30/05/2018",
                 executeScript("return arguments[0].value", displayText));
 
-        LogEntries logs  =  driver.manage().logs().get("browser");
 
-        Assert.assertEquals("Expected only [Deprecation] warning should be in the logs", 1, logs.filter(Level.WARNING).size());
+        assertBrowserLogs();
 
-        localePicker = findElement(By.id("german-locale-date-picker"));
-        executeScript("arguments[0].value = '10.01.1985'", localePicker);
+        localePicker = $(TestBenchElement.class).id("german-locale-date-picker");
+        executeScript("arguments[0].value = '10.01.1985';", localePicker);
 
-        logs  =  driver.manage().logs().get("browser");
+        assertBrowserLogs();
 
-        Assert.assertEquals("Expected only [Deprecation] warning should be in the logs", 1, logs.filter(Level.WARNING).size());
-
-        displayText = findInShadowRoot(localePicker, By.id("input")).get(0);
-        Assert.assertEquals("Didn't have expected German locale date.", "23.04.2018",
+        displayText = localePicker.$(TestBenchElement.class).id("input");
+        Assert.assertEquals("Didn't have expected German locale date.",
+                "10.01.1985",
                 executeScript("return arguments[0].value", displayText));
 
+    }
+
+    private void assertBrowserLogs() {
+        LogEntries logs = driver.manage().logs().get("browser");
+
+        Assert.assertEquals(
+                "Expected only [Deprecation] warning should be in the logs", 1,
+                logs.filter(Level.WARNING).size());
+        Assert.assertEquals(
+                "deprecation - Styling master document from stylesheets defined in HTML Imports is deprecated. Please refer to https://goo.gl/EGXzpw for possible migration paths.",
+                logs.filter(Level.WARNING).get(0).getMessage());
     }
 }
