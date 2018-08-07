5496803cfcfcfedaaf9356acba64311c01640870
Denis Anisimov <denis@vaadin.com>
2018-08-07 10:34:57 +0300

    Remove exclusion since the main serializable test is fixed

--- src/test/java/com/vaadin/flow/component/datepicker/DatePickerSerializableTest.java
+++ src/test/java/com/vaadin/flow/component/datepicker/DatePickerSerializableTest.java
@@ -1,19 +1,11 @@
 package com.vaadin.flow.component.datepicker;
 
-import java.util.stream.Stream;
-
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.testutil.ClassesSerializableTest;
 
 public class DatePickerSerializableTest extends ClassesSerializableTest {
 
     private static final UI FAKE_UI = new UI();
-
-    @Override
-    protected Stream<String> getExcludedPatterns() {
-        return Stream.concat(super.getExcludedPatterns(), Stream.of(
-                "com\\.vaadin\\.flow\\.component\\.datepicker\\.InjectedDatePickerI18nPage"));
-    }
 
     @Override
     protected void resetThreadLocals() {
